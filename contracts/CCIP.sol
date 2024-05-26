6f0b9fe5acb853318266a6daf7ec14eae38a77ea
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2024-05-26 22:24:04 +0100

    removed console from ccip

--- contracts/CCIP.sol
+++ contracts/CCIP.sol
@@ -6,7 +6,6 @@ import { OwnerIsCreator } from "@chainlink/contracts-ccip/src/v0.8/shared/access
 import { Client } from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
 import { IERC20 } from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
 import { SafeERC20 } from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/utils/SafeERC20.sol";
-import "hardhat/console.sol";
 
 contract SubscriptionTokenBridge is OwnerIsCreator {
     using SafeERC20 for IERC20;
