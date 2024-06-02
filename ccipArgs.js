42009255db80c318b92c0f0310bf1cb1df72e1b3
Isaac Adewumi <ienioladewumi@gmail.com>
2024-06-02 13:49:03 +0100

    verify ccip contractt

---
+++ ccipArgs.js
@@ -0,0 +1,5 @@
+module.exports = [
+  '0x9C32fCB86BF0f4a1A8921a9Fe46de3198bb884B2', // amoy ccip router
+  '0x0Fd9e8d3aF1aaee056EB9e802c3A762a667b1904', // amoy link token contract
+  ['14767482510784806043', '13264668187771770619', '16015286601757825753', '5224473277236331295'], // destination chain selectors
+];
